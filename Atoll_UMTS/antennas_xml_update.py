c0fa9ea3ad54c8e38acc12979399cde3d05836dd
dassw <sdbit04@gmail.com>
2019-07-09 20:16:34 +0530

    removed window specific command

--- Atoll_UMTS/antennas_xml_update.py
+++ Atoll_UMTS/antennas_xml_update.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as et
 import inspect
 import logging
 import datetime
+import shutil
 
 
 def create_custom_logger(log_level):
@@ -130,5 +131,60 @@ class AtollXmlConverter(object):
             excluded.write("antennas.xml\nPROFILESTRANSLATOR03.txt")
         # Created the excluded list above, used the file at below
         os.system("xcopy {}\*.* {} /y /d /exclude:{}".format(in_dir, out_dir, excluded_files))
-        os.system("del {0}\*.tmp, {0}\*_tmp.xml".format(out_dir))
+        # os.system("del {0}\*.tmp, {0}\*_tmp.xml".format(out_dir))
 
+    @classmethod
+    def copy_remaining_files(cls, in_dir, out_dir):
+        l1 = os.listdir(in_dir)
+        l2 = []
+        for d in l1:
+            if d == "antennas.xml" or d == "PROFILESTRANSLATOR03.txt":
+                pass
+            else:
+                l2.append(d)
+        os.chdir(in_dir)
+        for f in l2:
+            print("{0}\\{1}".format(out_dir, f))
+            shutil.copy(f, "{0}\\{1}".format(out_dir, f))
+
+
+if __name__ == "__main__":
+    origReplacement = {"CUSTOM_NEMS_ANTENNA_NAME": "FAMILY", "z:row": "z_row", "rs:data": "rs_data",
+                       "rs:insert": "rs_insert", "rs:nullable": "rs_nullable", "rs:fixedlength": "rs_fixedlength",
+                       "s:extends": "s_extends", "s:Schema": "s_Schema", "s:ElementType": "s_ElementType",
+                       "s:AttributeType": "s_AttributeType", "s:datatype": "s_datatype", "rs:updatable": "rs_updatable",
+                       "dt:type": "dt_type", "rs:number": "rs_number", "rs:write": "rs_write",
+                       "dt:maxLength": "dt_maxLength", "rs:precision": "rs_precision", "rs:maybenull": "rs_maybenull",
+                       "rs:long": "rs_long", "xmlns:s": "xmlns_s", "xmlns:dt": "xmlns_dt", "xmlns:rs": "xmlns_rs",
+                       "xmlns:z": "xmlns_z"}
+    replacementOrig = {"z_row": "z:row", "rs_data": "rs:data", "rs_insert": "rs:insert", "rs_nullable": "rs:nullable",
+                       "rs_fixedlength": "rs:fixedlength", "s_extends": "s:extends", "s_Schema": "s:Schema",
+                       "s_ElementType": "s:ElementType", "s_AttributeType": "s:AttributeType",
+                       "s_datatype": "s:datatype", "rs_updatable": "rs:updatable", "dt_type": "dt:type",
+                       "rs_number": "rs:number", "rs_write": "rs:write", "dt_maxLength": "dt:maxLength",
+                       "rs_precision": "rs:precision", "rs_maybenull": "rs:maybenull", "rs_long": "rs:long",
+                       "xmlns_s": "xmlns:s", "xmlns_dt": "xmlns:dt", "xmlns_rs": "xmlns:rs", "xmlns_z": "xmlns:z"}
+    xml_dir_in = ""
+    xml_dir_out = ""
+    with open("antennas_xml_update.properties", "r") as properties:
+        for line in properties.readlines():
+            if str(line)[0:10] == "xml_dir_in":
+                xml_dir_in = str(line).split(sep="=")[1].rstrip("\n")
+
+            if str(line)[0:11] == "xml_dir_out":
+                xml_dir_out = str(line).split(sep="=")[1].rstrip("\n")
+
+    print("input dir=" + xml_dir_in)
+    print("output dir=" + xml_dir_out)
+
+    if not os.path.isdir(xml_dir_in):
+        print("Please provide valid input directory")
+        exit()
+    if not os.path.isdir(xml_dir_out):
+        print("Please provide valid output directory")
+        exit()
+
+    # os.system("del {}\*".format(xml_dir_out))
+    antenna_n_family = AtollXmlConverter.beautify_family_attr(origReplacement,replacementOrig,xml_dir_in, xml_dir_out)
+    AtollXmlConverter.create_profile_translator(xml_dir_in, xml_dir_out, antenna_n_family)
+    AtollXmlConverter.copy_remaining_files(xml_dir_in, xml_dir_out)
